b13943f39c0f599fa0ee8c0a6b7a7ca67ac578d3
WindWalk <nhygta@hotmail.com>
2023-03-22 12:51:46 +0100

    Lavet Warehouse

--- src/application/model/Warehouse.java
+++ src/application/model/Warehouse.java
@@ -1,4 +1,42 @@
 package application.model;
 
+import java.util.ArrayList;
+
 public class Warehouse {
+    private final String name;
+    private final String address;
+    ArrayList<StorageRack> storageRacks = new ArrayList<>();
+    private int availableSpaces;
+
+    public Warehouse(String name, String address) {
+        this.name = name;
+        this.address = address;
+        this.availableSpaces = 0;
+    }
+
+    public void createStorageRack(String id, int width, int height) {
+        StorageRack sr = new StorageRack(id, width, height);
+        storageRacks.add(sr);
+        availableSpaces += width*height;
+    }
+
+    public void removeStorageRack(StorageRack s) {
+        storageRacks.remove(s);
+    }
+
+    public void reduceSpace() {
+        availableSpaces--;
+    }
+
+    public void increaseSpace() {
+        availableSpaces++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
