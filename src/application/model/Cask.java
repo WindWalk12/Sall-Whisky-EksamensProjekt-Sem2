cb34e9a9126bfcddd3418cfef4784f0cf98f6e86
Lasse Calvillo <eaalcnl@students.eaaa.dk>
2023-03-22 12:49:46 +0100

    Cask with casktype

--- src/application/model/Cask.java
+++ src/application/model/Cask.java
@@ -2,17 +2,18 @@ package application.model;
 
 public class Cask {
     private String id;
-    //TODO CAST TYPE
+    private CaskType caskType;
     private String countryOfOrigin;
     private String supplier;
     private double volume;
     private StorageRack storageRack;
 
-    public Cask(String id, String countryOfOrigin, String supplier, double volume) {
+    public Cask(String id, String countryOfOrigin, String supplier, double volume, CaskType caskType) {
         this.id = id;
         this.countryOfOrigin = countryOfOrigin;
         this.supplier = supplier;
         this.volume = volume;
+        this.caskType = caskType;
     }
 
     public String getId(){
