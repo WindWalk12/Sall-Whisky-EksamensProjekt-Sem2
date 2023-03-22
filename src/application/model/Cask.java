b855dbd75bb45d859cd196a1eb7a0a5e3984b2e8
Lasse Calvillo <eaalcnl@students.eaaa.dk>
2023-03-22 12:48:16 +0100

    Cask

--- src/application/model/Cask.java
+++ src/application/model/Cask.java
@@ -1,4 +1,53 @@
 package application.model;
 
 public class Cask {
+    private String id;
+    //TODO CAST TYPE
+    private String countryOfOrigin;
+    private String supplier;
+    private double volume;
+    private StorageRack storageRack;
+
+    public Cask(String id, String countryOfOrigin, String supplier, double volume) {
+        this.id = id;
+        this.countryOfOrigin = countryOfOrigin;
+        this.supplier = supplier;
+        this.volume = volume;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public String getCountryOfOrigin() {
+        return countryOfOrigin;
+    }
+
+    public void setCountryOfOrigin(String countryOfOrigin) {
+        this.countryOfOrigin = countryOfOrigin;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public StorageRack getStorageRack() {
+        return storageRack;
+    }
+
+    public void setStorageRack(StorageRack storageRack) {
+        this.storageRack = storageRack;
+    }
 }
