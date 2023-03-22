bf0ba1694710fd79af8322333fad4db7c91ad573
Lasse Calvillo <eaalcnl@students.eaaa.dk>
2023-03-22 13:16:00 +0100

    Controller basic functions

--- src/application/controller/Controller.java
+++ src/application/controller/Controller.java
@@ -1,4 +1,30 @@
 package application.controller;
 
+import application.model.Cask;
+import application.model.CaskType;
+import application.model.Warehouse;
+import storage.Storage;
+
+import java.util.ArrayList;
+
 public class Controller {
+    public static Warehouse createWarehouse(String name, String address){
+        Warehouse warehouse = new Warehouse(name, address);
+        Storage.addWarehouse(warehouse);
+        return warehouse;
+    }
+
+    public static ArrayList<Warehouse> getWarehouses(){
+        return Storage.getWarehouses();
+    }
+
+    public static Cask createCask(String id, String countryOfOrigin, String supplier, double volume, CaskType caskType){
+        Cask cask = new Cask(id, countryOfOrigin,supplier,volume,caskType);
+        Storage.addCask(cask);
+        return cask;
+    }
+
+    public static ArrayList<Cask> getCasks(){
+        return Storage.getCasks();
+    }
 }
