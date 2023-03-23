6043bd3e4b90f4fda8d7d4eb1a5c107af8761790
simon <simon_bruslund@hotmail.com>
2023-03-23 09:20:27 +0100

    Tilføjede knap funktionalitet til CaskPane samt MoveCaskWindow

--- src/gui/MoveCaskWindow.java
+++ src/gui/MoveCaskWindow.java
@@ -3,6 +3,7 @@ package gui;
 import application.model.Cask;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -50,5 +51,23 @@ public class MoveCaskWindow extends Stage {
         cbxStorageRacks = new ComboBox<>();
         pane.add(cbxStorageRacks, 2, 1);
 
+        // Buttons which makes it possible to save or exit
+
+        Button btnMove = new Button("Flyt til lager");
+        pane.add(btnMove, 1, 2);
+        btnMove.setOnAction(event -> this.moveAction());
+
+        Button btnExit = new Button("Luk");
+        pane.add(btnExit, 2, 2);
+        btnExit.setOnAction(event -> this.exitAction());
+    }
+
+    private void moveAction() {
+
+        this.hide();
+    }
+
+    private void exitAction() {
+        this.hide();
     }
 }
