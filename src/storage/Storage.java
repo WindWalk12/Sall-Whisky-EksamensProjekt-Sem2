c6e1a37c0d4b1fdb5d72c0c030bd0a3bc5410a0a
Lasse Calvillo <eaalcnl@students.eaaa.dk>
2023-03-27 10:42:57 +0200

    Distillation class

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -1,6 +1,8 @@
 package storage;
 
 import application.model.Cask;
+import application.model.Distillation;
+import application.model.Maltbatch;
 import application.model.Warehouse;
 
 import java.util.ArrayList;
@@ -10,6 +12,10 @@ public class Storage {
     private static ArrayList<Warehouse> warehouses = new ArrayList<>();
 
     private static ArrayList<Cask> casks = new ArrayList<>();
+
+    private static ArrayList<Maltbatch> maltbatches = new ArrayList<>();
+
+    private static ArrayList<Distillation> distillations = new ArrayList<>();
 
     //--------------------------------------------------------------------------------
     // Warehouse
@@ -27,7 +33,6 @@ public class Storage {
 
     //--------------------------------------------------------------------------------
     // Cask
-
     public static ArrayList<Cask> getCasks() {
         return new ArrayList<>(casks);
     }
@@ -38,5 +43,33 @@ public class Storage {
 
     public static void removeCask(Cask cask) {
         casks.remove(cask);
+    }
+
+    //--------------------------------------------------------------------------------
+    // Maltbatches
+    public static ArrayList<Maltbatch> getMaltbatches() {
+        return new ArrayList<>(maltbatches);
+    }
+
+    public static void addMaltbatch(Maltbatch maltbatch) {
+        maltbatches.add(maltbatch);
+    }
+
+    public static void removeMaltbatch(Maltbatch maltbatch) {
+        maltbatches.remove(maltbatch);
+    }
+
+    //--------------------------------------------------------------------------------
+    // distillations
+    public static ArrayList<Distillation> getDistillations() {
+        return new ArrayList<>(distillations);
+    }
+
+    public static void addDistillation(Distillation distillation) {
+        distillations.add(distillation);
+    }
+
+    public static void removeDistillation(Distillation distillation) {
+        distillations.remove(distillation);
     }
 }
