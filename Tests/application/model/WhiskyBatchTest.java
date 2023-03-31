a1131b8eb061499474ce84486abd8c398b330b90
WindWalk <nhygta@hotmail.com>
2023-03-31 11:12:53 +0200

    Start af tests

---
+++ Tests/application/model/WhiskyBatchTest.java
@@ -0,0 +1,42 @@
+package application.model;
+
+import org.junit.jupiter.api.Test;
+
+class WhiskyBatchTest {
+
+    @Test
+    void createWhiskyBottles() {
+    }
+
+    @Test
+    void createHistoryString() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getWaterVolume() {
+    }
+
+    @Test
+    void getTotalVolume() {
+    }
+
+    @Test
+    void isCaskStrength() {
+    }
+
+    @Test
+    void getAlcPrercntage() {
+    }
+
+    @Test
+    void getWhiskyType() {
+    }
+
+    @Test
+    void getWhiskyBottles() {
+    }
+}
