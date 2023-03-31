c05629fc8ca699de4e10d485632431359dcc044d
WindWalk <nhygta@hotmail.com>
2023-03-31 11:42:15 +0200

    Start af tests

---
+++ Tests/application/model/CaskTest.java
@@ -0,0 +1,85 @@
+package application.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CaskTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getCountryOfOrigin() {
+    }
+
+    @Test
+    void setCountryOfOrigin() {
+    }
+
+    @Test
+    void getSupplier() {
+    }
+
+    @Test
+    void setSupplier() {
+    }
+
+    @Test
+    void getVolume() {
+    }
+
+    @Test
+    void setVolume() {
+    }
+
+    @Test
+    void getStorageRack() {
+    }
+
+    @Test
+    void getTbvWarehouseName() {
+    }
+
+    @Test
+    void getTbvStorageRackId() {
+    }
+
+    @Test
+    void getCaskType() {
+    }
+
+    @Test
+    void getContentVolume() {
+    }
+
+    @Test
+    void removeFromContentVolume() {
+    }
+
+    @Test
+    void getNewestDate() {
+    }
+
+    @Test
+    void setStorageRack() {
+    }
+
+    @Test
+    void getDistilates() {
+    }
+
+    @Test
+    void addDistilate() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
