454a44d84897b37667de5b004d80ce6355e777be
WindWalk <nhygta@hotmail.com>
2023-04-11 08:38:44 +0200

    Opdateret

--- Tests/application/model/CaskTest.java
+++ Tests/application/model/CaskTest.java
@@ -8,50 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class CaskTest {
 
     @Test
-    void getId() {
-    }
-
-    @Test
-    void getCountryOfOrigin() {
-    }
-
-    @Test
-    void setCountryOfOrigin() {
-    }
-
-    @Test
-    void getSupplier() {
-    }
-
-    @Test
-    void setSupplier() {
-    }
-
-    @Test
-    void getVolume() {
-    }
-
-    @Test
-    void setVolume() {
-    }
-
-    @Test
-    void getStorageRack() {
-    }
-
-    @Test
-    void getTbvWarehouseName() {
-    }
-
-    @Test
-    void getTbvStorageRackId() {
-    }
-
-    @Test
-    void getCaskType() {
-    }
-
-    @Test
     void getContentVolume() {
     }
 
@@ -61,21 +17,5 @@ class CaskTest {
 
     @Test
     void getNewestDate() {
-    }
-
-    @Test
-    void setStorageRack() {
-    }
-
-    @Test
-    void getDistilates() {
-    }
-
-    @Test
-    void addDistilate() {
-    }
-
-    @Test
-    void testToString() {
     }
 }
